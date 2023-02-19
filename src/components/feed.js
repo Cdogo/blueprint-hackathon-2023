c2ad0f5602d4b2f076641e3408fe3d80dbde3b74
Cdogo <john.tanaristy@gmail.com>
2023-02-19 15:14:04 -0500

    logic work

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from "react";
 
-import firebase from 'firebase/compat/app'
-
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { db } from "./firebase";
 
 import { collection, query, where, limit, orderBy, getDocs, startAfter } from "firebase/firestore";
@@ -9,24 +8,9 @@ import { collection, query, where, limit, orderBy, getDocs, startAfter } from "f
 import Post from "./post"
 
 export default function Feed(){
-    const [posts, setPosts] = useState([])
-
-    useEffect(() => {
-        const getData = async () => {
-            const data = await getDocs(query(collection(db, 'websites')))
-            let predata = []
-            data.forEach((doc) => {
-                predata.push({id:doc.id, ...doc.data()})
-            })
-            console.log(predata)
-            setPosts(predata)    
-            console.log(posts);
-            
-        }
-        getData()
-    }, [posts])
+    const [posts] = useCollectionData(query(collection(db, 'websites')), { idField: 'id' })
 
     return(<div>
-        {posts.length !== 0 && posts.map((post) => {return(<Post key = {post.id} username = {post.user} website = {post.link}/>)})}
+        {posts && posts.map((post) => {return(<Post key = {post.id} username = {post.user} website = {post.link}/>)})}
     </div>)
 }
