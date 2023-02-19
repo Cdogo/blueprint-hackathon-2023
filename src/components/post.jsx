8d3de0d4f38b28d12f47544ed6a9a22146b68c96
TobiIjiyemi <tobi.ijiyemi@gmail.com>
2023-02-19 15:06:30 -0500

    little formatting to the posts

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,13 +1,13 @@
 import { getDatabase } from "firebase/database";
 import React, { Component, useState} from 'react';
 import firebase from "./firebase";
+import Link from 'next/link';
 
-
-export default function Post({username, website}){
+export default function Post({username, website, id}) {
   return (
-    <div>
-      <iframe src = {website} title = "websiteLink"></iframe>
-      <p>User: {username}</p>
+    <div className="flex flex-row border-gray-300 rounded-lg border max-w-screen-lg w-full p-5 m-10 bg-white space-y-4">
+      <p class = "text-decoration-thickness: 4px">{username}</p>
+      <iframe class = "w-full" src = {website} title = "websiteLink"></iframe>
     </div>
   );
 }
