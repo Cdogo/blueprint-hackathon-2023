88a6ca2c1f7813d518bc1b170956fe0122e7c08b
Cdogo <john.tanaristy@gmail.com>
2023-02-19 16:20:22 -0500

    Funcitoning

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,13 +1,13 @@
-import { getDatabase } from "firebase/database";
-import React, { Component, useState} from 'react';
-import firebase from "./firebase";
+import Link from 'next/link';
+
+import React from 'react';
 
 
-export default function Post({username, website}){
+export default function Post({id, username, website}){
   return (
-    <div>
-      <iframe src = {website} title = "websiteLink"></iframe>
-      <p>User: {username}</p>
-    </div>
+    <Link href={`/view/${id}`} className = 'block max-w-screen-md w-full border border-gray-300 p-3 rounded-lg'>
+      <p>{username}</p>
+      <iframe src = {website} title = "websiteLink" className = 'w-full'></iframe>
+    </Link>
   );
 }
