51eee141a0a13da2ddd74a53f70b1d923d15cba3
Cdogo <john.tanaristy@gmail.com>
2023-02-19 16:59:50 -0500

    stylish baby

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 export default function Post({id, username, website}){
   return (
     <Link href={`/view/${id}`} className = 'block max-w-screen-md w-full border border-gray-300 p-3 rounded-lg'>
-      <p>{username}</p>
+      <p className = 'font-bold mb-3'>{username}</p>
       <iframe src = {website} title = "websiteLink" className = 'w-full'></iframe>
     </Link>
   );
