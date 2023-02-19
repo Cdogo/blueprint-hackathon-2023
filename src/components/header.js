a035071fcb1bb58ef3e22b4c1aa8898d0b03f021
Cdogo <john.tanaristy@gmail.com>
2023-02-19 10:04:46 -0500

    Style update with header

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Header(){
+    return(
+        <header className = {`w-screen ${inter.className}`}>
+            <div className = 'flex flex-row justify-between max-w-screen-xl md:px-5 m-auto'>
+                <h1 className = 'w-fit'>Website Reviews</h1>
+                <nav className = 'flex'>
+                    <a className = 'block' href = '/login'>Login</a>
+                    <a className = 'block' href = '/sign-up'>Sign Up</a>
+                </nav>
+            </div>
+        </header>)
+}
